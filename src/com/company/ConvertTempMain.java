777025f7417802a5ed6a5c5d47971c43a387e6c1
Mubasher <21694@student.dorset-college.ie>
2021-02-20 17:18:12 +0000

    first

---
+++ src/com/company/ConvertTempMain.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.swing.JFrame;
+
+class ConvertTempTest {
+
+    public static void main(String[] args) {
+
+        ConvertTemp app = new ConvertTemp ();
+        app.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        app.setSize (500, 150);
+        app.setVisible (true);
+
+    }
+
+}
